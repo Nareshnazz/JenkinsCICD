a77da6b26ab4285517c1911956f0fd146f6b3fc2
Nareshnazz <153879885+Nareshnazz@users.noreply.github.com>
2024-01-05 15:37:20 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "myVPC"
+  name = "Terraform"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1b"]
+  azs             = ["us-west-2b"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 }
