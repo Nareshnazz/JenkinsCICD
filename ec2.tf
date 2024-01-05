133dcbd5efe51dedbdc4bfa5bdfc353beeb60977
Nareshnazz <153879885+Nareshnazz@users.noreply.github.com>
2024-01-05 15:53:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "ec2demo"
+  name = "Terraform-demo"
 
   instance_type          = "t2.micro"
   key_name               = "aws-key"
